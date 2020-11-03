031cb86d1f1a6cbea892fccf20c17eae8230d847
Drew Thompson <drewallynthompson@gmail.com>
2020-11-03 07:12:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,16 +11,16 @@ module FormsLab
       erb :'pirates/new'
     end
     
-    post '/pirates' do
-      
-      @pirates = Pirate.new(params['pirates']['name'], params['pirates']['weight'], params['pirates']['height'])
-      params['pirates']['ships'].each do |ship_data|
-        @ships = Ship.new(ship_data["name"], ship_data["type"], ship_data["booty"])
+     post '/pirates' do
+      params["pirate"]["ships"].each do |ship_data|
+        Ship.new(ship_data["name"], ship_data["type"], ship_data["booty"])
       end
-        
-        @ships = Ships.all
-        
-        erb :'pirates/show'
+
+      @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
+
+      @ships = Ship.all
+
+      erb :'pirates/show'
     end
 
   end
