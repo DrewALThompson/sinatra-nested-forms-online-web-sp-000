9434918195da2de25e97483a7d040fae6f481064
Drew Thompson <drewallynthompson@gmail.com>
2020-11-03 06:42:55 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,17 +1,17 @@
 class Ship
   attr_accessor :name, :type, :booty
   
-  @@ships = []
+  @@SHIPS = []
   
   def initialize(args)
     @name = args[:name]
     @type = args[:type]
     @booty = args[:booty]
-    @@all << self
+    @@SHIPS << self
   end
   
   def self.all
-    @@ships
+    @@SHIPS
   end
   
   def self.clear 
